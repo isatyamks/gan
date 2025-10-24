99fae61941c9971b0a18981100b7b0a242463c15
isatyamks <isatyamks@gmail.com>
2025-10-24 14:32:00 +0530

    feat: implement dcgan generator

---
+++ src/_generator.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self, nz, ngf, nc=3):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.ConvTranspose2d(nz, ngf*8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf*8),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(ngf*8, ngf*4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf*4),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(ngf*4, ngf*2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf*2),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(ngf*2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        return self.net(z)
